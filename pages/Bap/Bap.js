ada995ba797ab6cc311ab007dc7ef5a4fbf6a5d8
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-04-22 15:41:58 +0700

    Update Bap.js

--- pages/Bap/Bap.js
+++ pages/Bap/Bap.js
@@ -1,24 +1,27 @@
 import { apiUrl } from '../../globals.js';
 import { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, TextInput, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 
-const statusData = {
+const statusDescriptions = {
   0: 'Menunggu di approve Leader',
   1: 'Menunggu di approve MV',
-  2: 'Selesai',
-  3: 'Di Tolak',
-};
+  2: 'Sudah di approve MV',
+  3: 'Sedang di siapkan',
+  4: 'Sedang di kirim',
+  5: 'Selesai',
+  6: 'Di Tolak',
+}
 
-const kategoriData = {
-  1: 'Pengiriman artikel',
-  2: 'Mutasi artikel',
-};
+const AbsiBap = ({ route, navigation }) => {
+  const [dataToShow, setDataToShow] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [activeButton, setActiveButton] = useState('Proses');
 
-const AbsiDetailBap = ({ route }) => {
-  const [totalItems, setTotalItems] = useState(0);
-  const [dataArtikel, setDataArtikel] = useState([]);
-  const { id, status, kategori, catatan, id_kirim, created_at } = route.params;
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+  };
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -28,63 +31,100 @@ const AbsiDetailBap = ({ route }) => {
     return `${day}/${month}/${year}`;
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const selectedIdBap = await AsyncStorage.getItem('selected_id');
-        const formData = new FormData();
-        formData.append('id_bap', selectedIdBap);
-        const response = await fetch(apiUrl + '/getBapDetail', { method: 'POST', body: formData });
-        const data = await response.json();
-        if (data.success) {
-          setDataArtikel(data.detail);
-        }
-      } catch (error) {
-        //
+  const handleCardClick = async (bapItem) => {
+    try {
+      await AsyncStorage.setItem('selected_id', bapItem.id);   
+      let detailScreen = 'DetailBap';
+      navigation.navigate(detailScreen, { 
+        id: bapItem.id,
+        id_kirim: bapItem.id_kirim,
+        kategori: bapItem.kategori,
+        status: bapItem.status,
+        catatan: bapItem.catatan,
+        created_at: formatDate(bapItem.created_at),
+      });
+    } catch (error) {
+      // 
+    }
+  };
+  
+  const filteredData = dataToShow.filter((data) => {
+    if (activeButton === 'Proses') {
+      return ['0', '1', '2', '3', '4'].includes(data.status);
+    } else if (activeButton === 'Selesai') {
+      return data.status === '5';
+    } else if (activeButton === 'Tolak') {
+      return data.status === '6';
+    }
+    return false;
+  });
+
+  const fetchData = async (id_toko) => {
+    try {
+      const formData = new FormData();
+      formData.append('id_toko', id_toko);
+      const response = await fetch(apiUrl + '/getBap', { method: 'POST', body: formData });
+      const responseData = await response.json();
+      if (responseData.success) {
+        setDataToShow(responseData.bap);
       }
-    };
-    fetchData();
-  }, [id]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    let total = 0;
-    dataArtikel.forEach((barang) => { total += parseInt(barang.updatedJumlah) });
-    setTotalItems(total);
-  }, [dataArtikel]);
+    const fetchDataBasedOnButton = async () => {
+      let selectedStoreId = null;
+      if (route.params && route.params.selectedStore) {
+        selectedStoreId = route.params.selectedStore.id_toko;
+      } else {
+        selectedStoreId = await AsyncStorage.getItem('id_toko');
+      }
+      if (selectedStoreId) {
+        fetchData(selectedStoreId);
+      }
+    };
+    fetchDataBasedOnButton();
+  }, [activeButton]);  
 
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <View style={styles.header}>
-          <View style={styles.headerRow}>
-            <Text style={styles.headerLabel}>{id_kirim}</Text>
-            <Text style={styles.headerLabel}>{formatDate(created_at)}</Text>
-          </View>
-          <View style={styles.headerColumn}>
-            <Text style={styles.headerLabel}>Kategori: {kategoriData[kategori]}</Text>
-          </View>
-          <View style={styles.headerColumn}>
-            <Text style={styles.headerLabel}>Status: {statusData[status]}</Text>
-          </View>
-          <View style={styles.headerColumn}>
-            <Text style={styles.headerLabel}>Catatan: {catatan}</Text>
-          </View>
+        <TextInput value={searchText} style={styles.search} selectionColor="black" autoCapitalize="characters" placeholder="Cari Kode Artikel ..." onChangeText={(text) => setSearchText(text)}/>
+        <View style={styles.buttonOption}>
+          <TouchableOpacity onPress={() => handleButtonClick('Proses')} style={[styles.buttonOff, activeButton === 'Proses' && styles.buttonOn]}>
+            <Text style={[styles.buttonOffText, activeButton === 'Proses' && styles.buttonOnText]}>DI PROSES</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleButtonClick('Selesai')} style={[styles.buttonOff, activeButton === 'Selesai' && styles.buttonOn]}>
+            <Text style={[styles.buttonOffText, activeButton === 'Selesai' && styles.buttonOnText]}>SELESAI</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => handleButtonClick('Tolak')} style={[styles.buttonOff, activeButton === 'Tolak' && styles.buttonOn]}>
+            <Text style={[styles.buttonOffText, activeButton === 'Tolak' && styles.buttonOnText]}>DI TOLAK</Text>
+          </TouchableOpacity>
         </View>
         <ScrollView style={styles.scrollView}>
-          {dataArtikel.map((barang, index) => (
-            <View key={index} style={[styles.card, barang.updatedJumlah === '20' ? styles.cardBackground : null]}>
-              <View style={styles.cardRow}>
-                <Text style={styles.headerText1}>{`${index + 1}.`}</Text>
-                <View style={styles.cardColumn}>
-                  <Text style={styles.headerText2}>{`${barang.kode}`}</Text>
-                  <Text style={styles.headerText2}>{`${barang.nama_produk}`}</Text>
-                  <Text style={styles.headerText2}>{`Quantity: ${barang.qty_awal}`} || {`Update: ${barang.qty_update}`}</Text>
-                </View>
+          {filteredData.length === 0 && <Text style={styles.cardEmpty}>TIDAK ADA DATA</Text>}
+          {filteredData.map((bapItem, index) => (
+            <TouchableOpacity key={index} onPress={() => handleCardClick(bapItem)} style={[styles.card, bapItem.status === '5' && { borderColor: 'green' }, bapItem.status === '6' && { borderColor: 'red' }]}>
+              <View style={styles.cardMain}>
+                <Text style={styles.cardText}>{`${bapItem.id_kirim}`}</Text>
+                <Text style={styles.cardText}>{`${formatDate(bapItem.created_at)}`}</Text>
               </View>
-            </View>
+              <View style={styles.cardMain}>
+                <Text style={styles.cardText}>{`Status: ${statusDescriptions[bapItem.status]}`}</Text>
+              </View>
+              <View style={styles.cardMain}>
+                <Text style={styles.cardText}>{`Catatan: ${bapItem.catatan}`}</Text>
+              </View>
+            </TouchableOpacity>
           ))}
         </ScrollView>
       </View>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BuatBap')}>
+        <MaterialCommunityIcons size={20} name="plus" color="white" />
+        <Text style={styles.buttonText}>BUAT BAP</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -101,53 +141,88 @@ const styles = StyleSheet.create({
     marginBottom: -20,
     backgroundColor: 'white',
   },
-  header: {
-    padding: 15,
+  search: {
+    padding: 10,
     marginTop: -10,
-    borderRadius: 10,
+    borderWidth: 2,
+    borderRadius: 25,
     marginBottom: 10,
-    textAlign: 'center',
-    backgroundColor: '#071952',
+    borderColor: 'grey',
+    backgroundColor: '#F7F7F7',
   },
-  headerRow: {
+  buttonOption: {
+    marginBottom: 10,
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'white',
     justifyContent: 'space-between',
   },
-  headerLabel: {
+  buttonOff: {
+    padding: 10,
+    width: '32%',
+    borderWidth: 2,
+    borderRadius: 25,
+    alignItems: 'center',
+    borderColor: '#071952',
+    backgroundColor: '#F7F7F7',
+  },
+  buttonOn: {
+    borderColor: 'white',
+    backgroundColor: '#071952',
+  },
+  buttonOffText: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  buttonOnText: {
     fontSize: 16,
     color: 'white',
     fontWeight: 'bold',
   },
-  headerColumn: {
+  scrollView: {
+    marginBottom: 70,
+  },
+  card: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginBottom: 10,
+    borderColor: '#071952',
+    backgroundColor: 'white',
+  },
+  cardMain: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  scrollView: {
-    marginBottom: 10,
-  },
-  card: {
-    width: '95%',
-    marginBottom: 5,
-    borderBottomWidth: 2,
-    borderBottomColor: '#071952',
-  },
-  cardRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  headerText1: {
+  cardText: {
     fontSize: 16,
-    marginLeft: 5,
     fontWeight: 'bold',
   },
-  cardColumn: {
-    flex: 1,
+  button: {
+    bottom: 10,
+    padding: 15,
+    width: '90%',
+    borderRadius: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#071952',
   },
-  headerText2: {
+  buttonText: {
+    flex: 1,
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  cardEmpty: {
+    padding: 32,
     fontSize: 16,
-    marginLeft: 20,
+    borderWidth: 2,
+    borderRadius: 10,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    borderColor: '#071952',
   },
 });
-export default AbsiDetailBap;
+export default AbsiBap;
