f4838113aafb82b178350f998293e2db62446c51
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-04-26 09:56:01 +0700

    Update DetailPo.js

--- pages/PermintaanArtikel/DetailPo.js
+++ pages/PermintaanArtikel/DetailPo.js
@@ -1,37 +1,49 @@
+import { apiUrl } from '../../globals.js';
 import {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {View, Text, FlatList, StyleSheet, ActivityIndicator} from 'react-native';
 
-const statusDescriptions = {
-  0: 'Menunggu di approve Leader',
-  1: 'Menunggu di approve MV',
-  2: 'Sudah di approve MV',
-  3: 'Sedang di siapkan',
-  4: 'Sedang di kirim',
-  5: 'Selesai',
-  6: 'Di Tolak',
+const statusData = {
+  0: 'Permintaan artikel belum disetujui TL',
+  1: 'Permintaan artikel belum disetujui MV',
+  2: 'Permintaan artikel sudah disetujui',
+  3: 'Permintaan artikel sedang diproses',
+  4: 'Permintaan artikel sedang dikirim',
+  5: 'Permintaan artikel selesai',
+  6: 'Permintaan artikel ditolak',
 };
 
 const AbsiDetailPO = () => {
   const [poData, setPoData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [detailPoData, setDetailPoData] = useState([]);
   const totalItems = detailPoData.reduce((total, barang) => total + parseInt(barang.qty), 0);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const selectedIdPo = await AsyncStorage.getItem('selected_id_po');
         const formData = new FormData();
         formData.append('id_po', selectedIdPo);
-        const apiUrl = 'https://globalindo-group.com/absi_demo/api/getPoDetail';
-        const response = await fetch(apiUrl, {method: 'POST', body: formData});
+        const response = await fetch(apiUrl + '/getPoDetail', {method: 'POST', body: formData});
         const data = await response.json();
-        console.log(`success: ${data.success}, message: ${data.message}`);
-        console.log(data.detail_po);
-        if (data.success) {setPoData(data.po); setDetailPoData(data.detail_po)}
+        if (data.success) {
+          setPoData(data.po);
+          setDetailPoData(data.detail_po)
+          setTimeout(() => {setIsLoading(false)}, 500);
+        }
       } catch (error) {
-        console.error(error);
-        //
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -40,42 +52,39 @@ const AbsiDetailPO = () => {
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <View style={styles.header}>
-          <View style={styles.headerRow}>
-            <Text style={styles.headerLeft}>{poData && poData.id}</Text>
-            <Text style={styles.headerRight}>{poData && new Date(poData.created_at).toLocaleDateString()}</Text>
-          </View>
-          <View style={styles.headerColumn}>
-            <Text style={styles.headerLeft}>{`Status: ${statusDescriptions[poData && poData.status]}`}</Text>
-          </View>
-        </View>
-        <View style={styles.label}>
-          <Text style={styles.labelText1}>No.</Text>
-          <Text style={styles.labelText2}>List Artikel</Text>
-          <Text style={styles.labelText3}>Jumlah</Text>
-        </View>
-        <ScrollView style={styles.scroll}>
-          {detailPoData.map((barang, index) => (
-            <View key={index} style={styles.card}>
-              <View style={styles.cardRow}>
-                <Text style={styles.cardText1}>{`${index + 1}.`}</Text>
-                <View style={styles.cardColumn}>
-                  <Text style={styles.cardText2}>{`${barang.kode}`}</Text>
-                  <Text style={styles.cardText2}>{`${barang.nama_produk}`}</Text>
-                </View>
-                <Text style={styles.cardText3}>{`${barang.qty}`}</Text>
+        {isLoading ? (
+          <View><ActivityIndicator size="large" color="#071952"/></View>
+        ) : (
+          <>
+            <View style={styles.header}>
+              <View style={styles.headerCard}>
+                <Text style={styles.headerCardText1}>{poData && poData.id}</Text>
+                <Text style={styles.headerCardText2}>{poData && formatDate(poData.created_at)}</Text>
+              </View>
+              <View style={styles.headerCard}>
+                <Text style={styles.headerCardText3}>{`Status: ${statusData[poData && poData.status]}`}</Text>
               </View>
             </View>
-          ))}
-        </ScrollView>
-        <View style={styles.footer}>
-          <Text style={styles.labelText}>Total Barang:</Text>
-          <Text style={styles.labelText}>{totalItems}</Text>
-        </View>
+            <FlatList
+              data={detailPoData}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <View style={styles.card}>
+                  <Text style={styles.cardText1}>{`${item.kode} / ${item.nama_produk}`}</Text>
+                  <Text style={styles.cardText2}>{`Jumlah: ${item.qty}`}</Text>
+                </View>
+              )}
+            />
+            <View style={styles.footer}>
+              <Text style={styles.labelText}>Total Artikel:</Text>
+              <Text style={styles.labelText}>{totalItems}</Text>
+            </View>
+          </>
+        )}
       </View>
     </View>
   );
-};
+};  
 
 const styles = StyleSheet.create({
   container: {
@@ -90,85 +99,53 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   header: {
-    padding: 15,
+    padding: 10,
     marginTop: -10,
     borderRadius: 10,
     marginBottom: 10,
-    textAlign: 'center',
     backgroundColor: '#071952',
   },
-  headerRow: {
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'white',
-    justifyContent: 'space-between',
-  },
-  headerColumn: {
+  headerCard: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  headerLeft: {
+  headerCardText1: {
+    fontSize: 16,
+    marginTop: -5,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  headerCardText2: {
+    fontSize: 16,
+    marginTop: -5,
+    color: 'white',
+  },
+  headerCardText3: {
     fontSize: 16,
     color: 'white',
-    textAlign: 'left',
-    fontWeight: 'bold',
-  },
-  headerRight: {
-    fontSize: 16,
-    color: 'white',
-    textAlign: 'right',
-    fontWeight: 'bold',
-  },
-  label: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
-    justifyContent: 'space-between',
-  },
-  labelText1: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  labelText2: {
-    fontSize: 16,
-    marginLeft: -100,
-    fontWeight: 'bold',
-  },
-  labelText3: {
-    fontSize: 16,
-    marginRight: 15,
-    fontWeight: 'bold',
+    marginBottom: -5,
   },
   scroll: {
     marginBottom: 10,
   },
   card: {
-    width: '95%',
-    marginBottom: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: '#071952',
-  },
-  cardRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  cardColumn: {
-    flex: 1,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginBottom: 10,
+    borderColor: '#071952',
   },
   cardText1: {
     fontSize: 16,
+    marginTop: -5,
+    fontWeight: 'bold',
   },
   cardText2: {
     fontSize: 16,
-    marginLeft: 50,
-  },
-  cardText3: {
-    fontSize: 16,
-    marginRight: 10,
+    marginBottom: -5,
   },
   footer: {
-    padding: 15,
+    padding: 10,
     borderRadius: 10,
     marginBottom: 10,
     flexDirection: 'row',
