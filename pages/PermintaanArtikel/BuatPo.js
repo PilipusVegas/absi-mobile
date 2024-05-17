eac19dfe4163a54817bcc4d3596c524d3cc30caf
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-05-17 16:24:52 +0700

    Update BuatPo.js

--- pages/PermintaanArtikel/BuatPo.js
+++ pages/PermintaanArtikel/BuatPo.js
@@ -1,11 +1,10 @@
-import {apiUrl} from '../../globals.js';
-import {useState, useEffect, useCallback} from 'react';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { apiUrl } from '../../globals.js';
+import { useState, useEffect, useCallback } from 'react';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {View, Text, FlatList, TextInput, StyleSheet, TouchableOpacity, ActivityIndicator} from 'react-native';
+import { View, Text, FlatList, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
 const AbsiBuatPO = ({ navigation }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const [dataArtikel, setDataArtikel] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [selectedCount, setSelectedCount] = useState(0);
@@ -39,52 +38,45 @@ const AbsiBuatPO = ({ navigation }) => {
   }, [selectedItems]);
 
   const fetchData = useCallback(async () => {
-    setIsLoading(true);
     try {
       const id_toko = await AsyncStorage.getItem('id_toko');
       const formData = new FormData();
       formData.append('id_toko', id_toko || '');
-      const response = await fetch(apiUrl + '/listProdukToko', {method: 'POST', body: formData});
+      const response = await fetch(`${apiUrl}/listProdukToko`, {method: 'POST', body: formData});
       const data = await response.json();
       if (Array.isArray(data)) {
         setDataArtikel(data);
         setFilteredDataArtikel(data);
-        setTimeout(() => {setIsLoading(false)}, 200);
       }
     } catch (error) {
       console.error('Error fetching data:', error);
-      setIsLoading(false);
     }
   }, []);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);  
 
   return (
     <View style={styles.container}>
       <View style={styles.form}>
         <TextInput value={searchQuery} selectionColor="black" style={styles.textInput} onChangeText={handleSearch} autoCapitalize="characters" placeholder="Cari ID Artikel ..."/>
-        {isLoading ? (
-          <ActivityIndicator size={24} color="#071952"/>
-        ) : (
-          <FlatList
-            style={styles.flatList}
-            data={filteredDataArtikel}
-            keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <TouchableOpacity style={styles.card} onPress={() => handleItemPress(item)}>
-                <View style={styles.cardRow}>
-                  <View style={styles.cardColumn}>
-                    <Text style={styles.cardText1}>{`${item.kode}`}</Text>
-                    <Text style={styles.cardText2}>{`${item.nama_produk}`}</Text>
-                  </View>
-                  <Text style={[styles.cardButton, { borderColor: selectedItems.some((selectedItem) => selectedItem.id === item.id) ? 'red' : '#071952' }]}>{selectedItems.some((selectedItem) => selectedItem.id === item.id) ? 'HAPUS' : 'TAMBAH'}</Text>
+        <FlatList
+          style={styles.flatList}
+          data={filteredDataArtikel}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.card} onPress={() => handleItemPress(item)}>
+              <View style={styles.cardRow}>
+                <View style={styles.cardColumn}>
+                  <Text style={styles.cardText1}>{`${item.kode}`}</Text>
+                  <Text style={styles.cardText2}>{`${item.nama_produk}`}</Text>
                 </View>
-              </TouchableOpacity>
-            )}
-          />
-        )}
+                <Text style={[styles.cardButton, { borderColor: selectedItems.some((selectedItem) => selectedItem.id === item.id) ? 'red' : '#071952' }]}>{selectedItems.some((selectedItem) => selectedItem.id === item.id) ? 'HAPUS' : 'TAMBAH'}</Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
         <TouchableOpacity onPress={handleNavigate} disabled={selectedCount === 0} style={[styles.buttonOn, selectedCount === 0 && styles.buttonOff]}>
           <Text style={styles.buttonText}>{selectedCount ? `${selectedCount} ARTIKEL TERPILIH` : '0 ARTIKEL TERPILIH'}</Text>
           <MaterialCommunityIcons size={24} color="white" name="arrow-right-bold"/>
@@ -131,7 +123,7 @@ const styles = StyleSheet.create({
   },
   cardColumn: {
     flex: 1,
-    marginRight: 5,
+    justifyContent: 'center',
   },
   cardText1: {
     fontSize: 16,
