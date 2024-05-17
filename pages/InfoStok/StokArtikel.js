89045879401f36676588fb45840b44f4e102e5a7
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-05-17 14:28:07 +0700

    Update StokArtikel.js

--- pages/InfoStok/StokArtikel.js
+++ pages/InfoStok/StokArtikel.js
@@ -1,81 +1,80 @@
+import { apiUrl } from '../../globals.js';
 import { useState, useEffect } from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, TextInput, StyleSheet, FlatList } from 'react-native';
 
-const AbsiStokArtikel = () => {
-  const [dataBarang ] = useState([
-    { id: '1', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '2', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '3', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '4', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '5', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '6', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '7', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-    { id: '8', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 3-1 (L)', jumlah: '10' },
-    { id: '9', kodeArtikel: 'FG-HXKL-205MX-C', namaArtikel: 'Hicoop Boxer Karet Luar Mix Max Seri 2-1 (L)', jumlah: '10' },
-  ]);
-  const [totalItems, setTotalItems] = useState(0);
+const AbsiStokArtikel = ({ route }) => {
+  const [idToko, setIdToko] = useState('');
+  const [dataBarang, setDataBarang] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredItems, setFilteredItems] = useState([]);
+
   const handleSearch = (text) => {
     setSearchQuery(text);
     const filtered = dataBarang.filter(
-      (item) => item.kodeArtikel.toLowerCase().indexOf(text.toLowerCase()) !== -1
+      (item) => item.kode.toLowerCase().includes(text.toLowerCase())
     );
     setFilteredItems(filtered);
   };
+
+  const fetchData = async (id_toko) => {
+    try {
+      const formData = new FormData();
+      formData.append('id_toko', id_toko);
+      const response = await fetch(`${apiUrl}/listProdukToko`, { method: 'POST', body: formData });
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const json = await response.json();
+      setDataBarang(json);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   useEffect(() => {
     setFilteredItems(dataBarang);
   }, [dataBarang]);
+
   useEffect(() => {
-    let total = 0;
-    filteredItems.forEach((barang) => {
-      total += parseInt(barang.jumlah);
-    });
-    setTotalItems(total);
-  }, [filteredItems]);
+    const fetchDataBasedOnParams = async () => {
+      if (route.params && route.params.selectedStore) {
+        const { selectedStore } = route.params;
+        setIdToko(selectedStore.id_toko);
+        await fetchData(selectedStore.id_toko);
+      } else {
+        const getStoredIdToko = async () => {
+          try {
+            const storedIdToko = await AsyncStorage.getItem('id_toko');
+            if (storedIdToko) {
+              setIdToko(storedIdToko);
+              await fetchData(storedIdToko);
+            }
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+        getStoredIdToko();
+      }
+    };
+    fetchDataBasedOnParams();
+  }, [route.params]);
 
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <View style={styles.labelColumn}>
-          <View style={styles.labelRow}>
-            <Text style={styles.labelText}>Total Artikel:</Text>
-            <Text style={styles.labelText}>{filteredItems.length}</Text>
-          </View>
-          <View style={styles.labelRow}>
-            <Text style={styles.labelText}>Total Stok Artikel:</Text>
-            <Text style={styles.labelText}>{totalItems}</Text>
-          </View>
-        </View>
-        <TextInput
-          value={searchQuery}
-          style={styles.search}
-          onChangeText={handleSearch}
-          placeholder="Cari kode barang..."
-        />
-        <View style={styles.label}>
-          <Text style={styles.labelText1}>No.</Text>
-          <Text style={styles.labelText2}>List Artikel</Text>
-          <Text style={styles.labelText3}>Jumlah</Text>
-        </View>
-        <ScrollView style={styles.scroll}>
-          {filteredItems.map((barang, index) => (
-            <View
-              key={index}
-              onPress={() => handleItemPress(barang)}
-              style={[styles.card, barang.jumlah === '20' ? styles.cardBackground : null]}
-            >
-              <View style={styles.cardRow}>
-                <Text style={styles.cardText1}>{`${barang.id}.`}</Text>
-                <View style={styles.cardColumn}>
-                  <Text style={styles.cardText2}>{`${barang.kodeArtikel}`}</Text>
-                  <Text style={styles.cardText3}>{`${barang.namaArtikel}`}</Text>
-                </View>
-                <Text style={styles.cardText5}>{`${barang.jumlah}`}</Text>
-              </View>
+        <TextInput value={searchQuery} selectionColor="black" style={styles.textInput} onChangeText={handleSearch} autoCapitalize="characters" placeholder="Cari ID Artikel ..."/>
+        <FlatList 
+          data={filteredItems} 
+          style={styles.flatList}
+          keyExtractor={(item) => item.id.toString()} 
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.cardText1}>{item.kode} / {item.nama_produk}</Text>
+              <Text style={styles.cardText2}>{`Jumlah: ${item.qty}`}</Text>
             </View>
-          ))}
-        </ScrollView>
+          )} 
+        />
       </View>
     </View>
   );
@@ -93,92 +92,33 @@ const styles = StyleSheet.create({
     marginBottom: -20,
     backgroundColor: 'white',
   },
-  labelColumn: {
+  textInput: {
     padding: 10,
     marginTop: -10,
+    borderWidth: 2,
     borderRadius: 10,
     marginBottom: 10,
-    flexDirection: 'column',
-    backgroundColor: '#071952',
-  }, 
-  labelRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    borderColor: 'grey',
+    backgroundColor: '#F7F7F7',
   },
-  labelText: {
-    fontSize: 16,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  search: {
-    padding: 10,
-    width: '100%',
-    borderWidth: 2,
-    marginBottom: 10,
-    borderRadius: 25,
-    alignSelf: 'center',
-    borderColor: '#071952',
-    backgroundColor: 'white',
-  },
-  label: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
-    justifyContent: 'space-between',
-  },
-  labelText1: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  labelText2: {
-    fontSize: 16,
-    marginLeft: -185,
-    fontWeight: 'bold',
-  },
-  labelText3: {
-    fontSize: 16,
-    marginRight: 15,
-    fontWeight: 'bold',
-  },
-  scroll: {
+  flatList: {
     marginBottom: 10,
   },
   card: {
-    width: '95%',
-    marginBottom: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: '#071952',
-  },
-  cardBackground: {
-    backgroundColor: 'red',
-  },
-  cardRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  cardColumn: {
-    flex: 1,
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginBottom: 10,
+    borderColor: '#071952',
   },
   cardText1: {
     fontSize: 16,
-    marginLeft: 5,
+    marginTop: -5,
     fontWeight: 'bold',
   },
   cardText2: {
     fontSize: 16,
-    marginLeft: 15,
-    fontWeight: 'bold',
-  },
-  cardText3: {
-    fontSize: 16,
-    marginLeft: 15,
-  },
-  cardText5: {
-    fontSize: 16,
-    marginRight: 10,
-    fontWeight: 'bold',
+    marginBottom: -5,
   },
 });
 export default AbsiStokArtikel;
