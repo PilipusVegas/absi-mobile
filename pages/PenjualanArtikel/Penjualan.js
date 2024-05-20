4caca2db0677c2a6b8a3dafcf5890295237c8db9
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-05-20 16:18:43 +0700

    Update Penjualan.js

--- pages/PenjualanArtikel/Penjualan.js
+++ pages/PenjualanArtikel/Penjualan.js
@@ -1,139 +1,93 @@
+import { apiUrl } from '../../globals.js';
 import { useState, useEffect } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, FlatList, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 
-const AbsiPenjualan = ({ navigation }) => {
-  const dataProses = [
-    { pj: 'PJ-202301001', tanggal: '01/01/2024' },
-    { pj: 'PJ-202301002', tanggal: '01/01/2024' },
-    { pj: 'PJ-202301003', tanggal: '01/01/2024' },
-    { pj: 'PJ-202301004', tanggal: '01/01/2024' },
-    { pj: 'PJ-202301005', tanggal: '01/01/2024' },
-    { pj: 'PJ-202301006', tanggal: '02/01/2024' },
-    { pj: 'PJ-202301007', tanggal: '02/01/2024' },
-    { pj: 'PJ-202301008', tanggal: '02/01/2024' },
-    { pj: 'PJ-202301009', tanggal: '02/01/2024' },
-  ];
-  const [searchText, setSearchText] = useState('');
+const AbsiPenjualan = ({ route, navigation }) => {
+  const [idToko, setIdToko] = useState(null);
   const [dataToShow, setDataToShow] = useState([]);
-  const [activeButton, setActiveButton] = useState(null);
-  const [selectedDate, setSelectedDate] = useState('dd/mm/yyyy');
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-  const filteredData = dataToShow.filter(
-    (data) => data.pj.toLowerCase().includes(searchText.toLowerCase())    
-  );
-  const handleResetButtonClick = () => {
-    setSelectedDate('DD/MM/YYYY');
-    setSearchText('');
-    handleButtonClick('Proses');
-  };
-  const handleCardClick = (data) => {
-    let detailScreen = 'DetailPenjualan';
-    navigation.navigate(detailScreen, { 
-      pj: data.pj,
-      tanggal: data.tanggal,
-    });
-  };
-  const handleButtonClick = (buttonName) => {
-    setActiveButton(buttonName);
-    if (buttonName === 'Proses') {
-      const sortedData = [...dataProses].sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal));
-      const slicedData = sortedData.slice(0, 10);
-      setDataToShow(slicedData);
-    }
-  };
-  const handleDateConfirm = (date) => {
-    setDatePickerVisibility(false);
-    filterAndSetData(date);
-    const day = ('0' + date.getDate()).slice(-2);
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const year = date.getFullYear();
-    const formattedDate = `${day}/${month}/${year}`;
-    setSelectedDate(formattedDate);
-  };
-  const filterAndSetData = (date) => {
-    const formattedDate = `${('0' + date.getDate()).slice(-2)}/${('0' + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`;
-    const filteredData = dataProses.filter(
-      (data) => {
-        const formattedDataDate = data.tanggal;
-        return formattedDataDate === formattedDate;
-      }
+  const [searchText, setSearchText] = useState('');
+
+  const filterDataById = (text) => {
+    const filteredData = dataToShow.filter((item) =>
+      item.id.toLowerCase().includes(text.toLowerCase())
     );
     setDataToShow(filteredData);
   };
+
+  const handleCardClick = async (data) => {
+    try {
+      if (data.id) {
+        await AsyncStorage.setItem('selected_id_penjualan', data.id);
+        navigation.navigate('DetailPenjualan', { pj: data.id });
+      }
+    } catch (error) {
+      console.error('Error handling card click:', error);
+    }
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
+
+  const fetchData = async (id_toko) => {
+    try {
+      const formData = new FormData();
+      formData.append('id_toko', id_toko);
+      const response = await fetch(apiUrl + '/getPenjualan', { method: 'POST', body: formData });
+      const responseData = await response.json();
+      if (responseData.success && responseData.penjualan) {
+        setDataToShow(responseData.penjualan);
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   useEffect(() => {
-    handleButtonClick('Proses');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  useEffect(() => {
-    setSelectedDate('dd/mm/yyyy');
-  }, []);
-  useEffect(() => {
-    const initialFilteredData = dataProses.filter(
-      (data) => new Date(data.tanggal).toDateString() === new Date().toDateString()
-    );
-    setDataToShow(initialFilteredData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  useEffect(() => {
-    const sortedData = [...dataProses].sort((a, b) => new Date(b.tanggal) - new Date(a.tanggal));
-    const slicedData = sortedData.slice(0, 10);
-    setDataToShow(slicedData);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    const fetchDataBasedOnParams = async () => {
+      if (route.params && route.params.selectedStore) {
+        const { selectedStore } = route.params;
+        setIdToko(selectedStore.id_toko);
+        await fetchData(selectedStore.id_toko);
+      } else {
+        const storedIdToko = await AsyncStorage.getItem('id_toko');
+        if (storedIdToko) {
+          setIdToko(storedIdToko);
+          await fetchData(storedIdToko);
+        }
+      }
+    };
+    fetchDataBasedOnParams();
+  }, [route.params]);
 
   return (
     <View style={styles.container}>
       <View style={styles.form}>
-        <View style={styles.search}>
-          <TextInput
-            value={searchText}
-            selectionColor="black"
-            style={styles.textInput}
-            autoCapitalize="characters"
-            placeholder="Cari Kode Artikel ..."
-            onChangeText={(text) => setSearchText(text)}
-          />
-          <View style={styles.calender}>
-            <TouchableOpacity onPress={showDatePicker} style={styles.calenderButton}>
-              <MaterialCommunityIcons size={20} color="white" name="calendar" marginRight={5}/>
-              <Text style={styles.calenderText}>{selectedDate === 'dd/mm/yyyy' ? 'DD/MM/YYYY' : selectedDate}</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.calenderButton} onPress={handleResetButtonClick}>
-              <Text style={styles.calenderReset}>RESET</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-        <DateTimePickerModal
-          mode="date"
-          onConfirm={handleDateConfirm}
-          isVisible={isDatePickerVisible}
-          onCancel={() => setDatePickerVisibility(false)}
-        />
-        <ScrollView style={styles.scroll}>
-          {filteredData.map((data, index) => (
-            <TouchableOpacity key={index} style={styles.card} onPress={() => handleCardClick(data)}>
-              <View style={styles.cardRow}>
-                <View style={styles.cardColumn}>
-                  <Text style={styles.cardText1}>{`${data.pj}`}</Text>
-                  <Text style={styles.cardText2}>{`Tanggal: ${data.tanggal}`}</Text>
-                </View>
-                <Text style={styles.cardButton}>
-                  {activeButton === 'Proses' ? 'DETAIL' : ''}
-                </Text>
+      <TextInput value={searchText} selectionColor="black" style={styles.textInput} autoCapitalize="characters" placeholder="Cari ID Pengiriman ..." onChangeText={(text) => setSearchText(text)}/>
+        <FlatList
+          style={styles.flatList}
+          keyExtractor={(item) => item.id.toString()}
+          data={dataToShow.filter((data) => data.id.includes(searchText))}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.card} onPress={() => handleCardClick(item)}>
+              <View style={styles.cardButton}>
+                <Text style={styles.cardText1}>{item.id}</Text>
+                <Text style={styles.cardText2}>{`${formatDate(item.created_at)}`}</Text>
               </View>
             </TouchableOpacity>
-          ))}
-        </ScrollView>
+          )}
+        />
       </View>
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BuatPenjualan')}>
-        <MaterialCommunityIcons name="plus" size={24} color="white" />
+        <MaterialCommunityIcons name="plus" size={24} color="white"/>
         <Text style={styles.buttonText}>Input Penjualan</Text>
-      </TouchableOpacity> 
+      </TouchableOpacity>
     </View>
   );
 };
@@ -150,43 +104,16 @@ const styles = StyleSheet.create({
     marginBottom: -20,
     backgroundColor: 'white',
   },
-  search: {
-    marginBottom: 10,
-  },
   textInput: {
     padding: 10,
     marginTop: -10,
     borderWidth: 2,
-    borderRadius: 25,
+    borderRadius: 10,
     marginBottom: 10,
     borderColor: 'grey',
     backgroundColor: '#F7F7F7',
   },
-  calender: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  calenderButton: {
-    padding: 10,
-    width: '49%',
-    borderRadius: 25,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    backgroundColor: '#071952',
-  },
-  calenderText: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  calenderReset: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  scroll: {
+  flatList: {
     marginBottom: 75,
   },
   card: {
@@ -195,14 +122,11 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 10,
     borderColor: '#071952',
-    backgroundColor: '#fff',
+    backgroundColor: '#F7F7F7',
   },
-  cardRow: {
+  cardButton: {
     flexDirection: 'row',
-  },
-  cardColumn: {
-    flex: 1,
-    marginRight: 5,
+    justifyContent: 'space-between',
   },
   cardText1: {
     fontSize: 16,
@@ -211,18 +135,7 @@ const styles = StyleSheet.create({
   },
   cardText2: {
     fontSize: 16,
-    marginBottom: -5,
-  },
-  cardButton: {
-    padding: 10,
-    fontSize: 16,
-    color: 'black',
-    borderWidth: 2,
-    borderRadius: 10,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    alignSelf: 'center',
-    backgroundColor: '#F7F7F7',
+    marginTop: -5,
   },
   button: {
     bottom: 10,
