f7ba928e779da4d0898bb80b8cde154840a7cd99
PilipusVegas <53340059+PilipusVegas@users.noreply.github.com>
2024-05-20 10:25:10 +0700

    Update DetailRetur.js

--- pages/ReturArtikel/DetailRetur.js
+++ pages/ReturArtikel/DetailRetur.js
@@ -1,6 +1,6 @@
 import { apiUrl } from '../../globals.js';
-import {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const statusDescriptions = {
@@ -16,7 +16,6 @@ const statusDescriptions = {
 const AbsiDetailRetur = () => {
   const [returData, setReturData] = useState(null);
   const [detailReturData, setDetailReturData] = useState([]);
-  const totalItems = detailReturData.reduce((total, barang) => total + parseInt(barang.qty), 0);
 
   const formatDate = (dateString) => {
     const date = new Date(dateString);
@@ -32,14 +31,14 @@ const AbsiDetailRetur = () => {
         const selectedIdRetur = await AsyncStorage.getItem('selected_id');
         const formData = new FormData();
         formData.append('id_retur', selectedIdRetur);
-        const response = await fetch(apiUrl + '/getReturDetail', { method: 'POST', body: formData });
+        const response = await fetch(`${apiUrl}/getReturDetail`, { method: 'POST', body: formData });
         const data = await response.json();
         if (data.success) {
           setReturData(data.retur);
           setDetailReturData(data.detail);
         }
       } catch (error) {
-        //
+        console.error('Error fetching data:', error);
       }
     };
     fetchData();
@@ -49,37 +48,25 @@ const AbsiDetailRetur = () => {
     <View style={styles.container}>
       <View style={styles.form}>
         <View style={styles.header}>
-          <View style={styles.headerRow}>
-            <Text style={styles.headerLeft}>{returData && returData.id}</Text>
-            <Text style={styles.headerRight}>{returData && formatDate(returData.created_at)}</Text>
+          <View style={styles.headerCard}>
+            <Text style={styles.headerCardText1}>{returData && returData.id}</Text>
+            <Text style={styles.headerCardText2}>{returData && formatDate(returData.created_at)}</Text>
           </View>
-          <View style={styles.headerColumn}>
-            <Text style={styles.headerLeft}>{`Status: ${statusDescriptions[returData && returData.status]}`}</Text>
+          <View style={styles.headerCard}>
+            <Text style={styles.headerCardText3}>{`Status: ${statusDescriptions[returData && returData.status]}`}</Text>
           </View>
         </View>
-        <View style={styles.label}>
-          <Text style={styles.labelText1}>No.</Text>
-          <Text style={styles.labelText2}>List Artikel</Text>
-          <Text style={styles.labelText3}>Jumlah</Text>
-        </View>
-        <ScrollView style={styles.scrollView}>
-          {detailReturData.map((barang, index) => (
-            <View key={index} style={styles.card}>
-              <View style={styles.cardRow}>
-                <Text style={styles.cardText1}>{`${index + 1}.`}</Text>
-                <View style={styles.cardColumn}>
-                  <Text style={styles.cardText2}>{`${barang.kode}`}</Text>
-                  <Text style={styles.cardText2}>{`${barang.nama_produk}`}</Text>
-                </View>
-                <Text style={styles.cardText3}>{`${barang.qty}`}</Text>
-              </View>
+        <FlatList
+          data={detailReturData}
+          style={styles.flatList}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Text style={styles.cardText1}>{`${item.kode} / ${item.nama_produk}`}</Text>
+              <Text style={styles.cardText2}>{`Jumlah: ${item.qty}`}</Text>
             </View>
-          ))}
-        </ScrollView>
-        <View style={styles.footer}>
-          <Text style={styles.footerLabel}>Total Barang:</Text>
-          <Text style={styles.footerLabel}>{totalItems}</Text>
-        </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -98,95 +85,47 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   header: {
-    padding: 15,
+    padding: 10,
     marginTop: -10,
     borderRadius: 10,
     marginBottom: 10,
-    textAlign: 'center',
     backgroundColor: '#071952',
   },
-  headerRow: {
+  headerCard: {
     flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'white',
     justifyContent: 'space-between',
   },
-  headerLeft: {
+  headerCardText1: {
+    fontSize: 16,
+    marginTop: -5,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  headerCardText2: {
+    fontSize: 16,
+    marginTop: -5,
+    color: 'white',
+  },
+  headerCardText3: {
     fontSize: 16,
     color: 'white',
-    textAlign: 'left',
-    fontWeight: 'bold',
-  },
-  headerRight: {
-    fontSize: 16,
-    color: 'white',
-    textAlign: 'right',
-    fontWeight: 'bold',
-  },
-  headerColumn: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  label: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    borderBottomWidth: 1,
-    borderBottomColor: 'black',
-    justifyContent: 'space-between',
-  },
-  labelText1: {
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  labelText2: {
-    fontSize: 16,
-    marginLeft: -100,
-    fontWeight: 'bold',
-  },
-  labelText3: {
-    fontSize: 16,
-    marginRight: 15,
-    fontWeight: 'bold',
-  },
-  scrollView: {
-    marginBottom: 10,
+    marginBottom: -5,
   },
   card: {
-    width: '95%',
-    marginBottom: 5,
-    borderBottomWidth: 1,
-    borderBottomColor: '#071952',
-  },
-  cardRow: {
-    alignItems: 'center',
-    flexDirection: 'row',
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    marginBottom: 10,
+    borderColor: '#071952',
   },
   cardText1: {
     fontSize: 16,
-  },
-  cardColumn: {
-    flex: 1,
+    marginTop: -5,
+    fontWeight: 'bold',
   },
   cardText2: {
     fontSize: 16,
-    marginLeft: 50,
-  },
-  cardText3: {
-    fontSize: 16,
-    marginRight: 10,
-  },
-  footer: {
-    padding: 15,
-    borderRadius: 10,
-    marginBottom: 10,
-    flexDirection: 'row',
-    backgroundColor: '#071952',
-    justifyContent: 'space-between',
-  },
-  footerLabel: {
-    fontSize: 16,
-    color: 'white',
-    fontWeight: 'bold',
+    marginBottom: -5,
   },
 });
 export default AbsiDetailRetur;
